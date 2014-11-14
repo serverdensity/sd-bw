fbe4a81a4bc49c5d49645b672b981213c90d9903
Jonathan Sundqvist <sundqvist.jonathan@gmail.com>
2014-11-14 12:11:13 +0000

    Various changes for script

--- bandwidth/__init__.py
+++ bandwidth/__init__.py
@@ -2,6 +2,7 @@ import json
 from collections import namedtuple
 from urlparse import urljoin
 import os.path
+import sys
 
 import requests
 
@@ -10,6 +11,8 @@ import requests
 
 BASE_URL = 'https://api.serverdensity.io'
 CONFIG_PATH = '~/.config.json'
+
+Bandwidth = namedtuple('Bandwidth', 'rxgb, txgb')
 
 
 def parse_response(response):
@@ -21,20 +24,20 @@ def parse_response(response):
 		if response.status_code == 200:
 			return data
 		elif not data:
-			data['message'] = 'Dictionary is empty'
+			data['message'] = 'There was no response'
 			raise Exception
 		elif response.status != 200:
 			raise Exception
 	except Exception:
 		print 'Error: {0}'.format(data['message'])
+		# exit to os. 
 
 def get_jsondata(urlpath, payload):
 	response = requests.get(urljoin(BASE_URL, urlpath), params=payload)
 	return parse_response(response)
 
 
-def available_metrics():
-	config = read_config()	
+def available_metrics(config):	
 	payload = {
 		        'token': config['api_key'],
 		        'start' : config['start'],
@@ -45,14 +48,12 @@ def available_metrics():
 
 	return api_response
 
-def bandwidth_response():
+def bandwidth_response(config):
+	# Note: from groups I would like to set device id and interface. 
 	"""Get total_bandwidth for current device and current interface"""
-	config = read_config()
 
 	filters = {
-    'networkTraffic': {
-        config['current_interface']: ['rxMByteS', 'txMByteS']
-    	}
+    	'networkTraffic': ['rxMByteS', 'txMByteS']
 	}
 
 	payload = {
@@ -74,7 +75,7 @@ def available_devices():
 	return api_response
 
 def get_network_interfaces(metrics):
-	"""Gives you a list of network adapters"""
+	"""Gives you a list of network adapters from available_metrics"""
 	network = {}
 	adapters = []
 	for j in metrics:
@@ -87,25 +88,73 @@ def get_network_interfaces(metrics):
 
 	return adapters
 
-def get_groups(device_response):
+
+def update_groups():
+	device_response = available_devices()
+	config = read_config()
+	if not config['devices']:
+		print "No devices found in config file, updating devices..."
+		update_devices()
+		config = read_config()
+	
 	groupdic = {}
 	for device in device_response:
-		devicedic[device['group']] = { device['name']: device['_id']}
-	return groupdic
+		groupdic[device['group']] = { device['name']: {
+										'_id': device['_id'],
+										'interface': config['devices'][device['name']]['interfaces']
+										}
+									}
+
+	print "Updated groups and saved it to config files."
+	config['groups'] = groupdic
+	modify_config(config)
+
+def print_groups():
+	config = read_config()
+	if not config['groups']:
+		print "There are no groups"
+	for groupname, groupdic in config['groups'].iteritems():
+		print "Group: {}".format(groupname)
+		for device, devicedic in groupdic.iteritems():
+			print "	{}".format(device)
+		print "\n"
+
+
 
 def get_devices(device_response):
 	devicedic = {}
 	for device in device_response:
-		devicedic[device['name']] =  device['_id']
+		devicedic[device['name']] =  {'_id': device['_id']}
 	return devicedic
 
-def present_devices():
+def get_interfaces(bandwidth_response):
+	interfaces = bandwidth_response[0]['tree']
+	interfaces_list = [interface['name'] for interface in interfaces]
+	return interfaces_list
+
+def print_devices():
 	devices = available_devices()
 	devicedic = get_devices(devices)
 
-	for name, _id in devicedic.iteritems():
-		print "Device: {0} 		ID: {1}".format(name, _id)
+	for name, dic in devicedic.iteritems():
+		print """
+		Device: {0}
+		ID: {1}
+		Interfaces: {2}""".format(name, dic['_id'], ", ".join(dic['interfaces']))
 
+
+def update_devices():
+	response = available_devices()
+	devices = get_devices(response)
+	config = read_config()
+	for name, dic in devices.iteritems():
+		config['current_device'] = dic['_id']
+		response = bandwidth_response(config)
+		interfaces = get_interfaces(response)
+		devices[name].update({'interfaces': interfaces})
+	config['devices'] = devices
+	modify_config(config)
+	print "Updated devices and saved it to config files."
 
 def read_config():
 	config = {}
@@ -122,28 +171,104 @@ def modify_config(keydic):
 		config[key] = val
 	with open(os.path.expanduser(CONFIG_PATH), 'w') as fp:
 		json.dump(config, fp)
+	return config
 
-def calculate_bandwidth(trafficdic):
-	rxmblist = trafficdic[0]['tree'][0]['tree'][0]['data']
-	txmblist = trafficdic[0]['tree'][0]['tree'][1]['data']
+def print_bandwidth_group(groupname, start=None, end=None):
+	if start:
+		modify_config({'start': start, 'end': end})
+
+	config = read_config()
+	group = config['groups'][groupname]
+
+	group = calc_bandwidth_group(groupname)
+
+	print "\n{0} 	{1} 	{2} 	{3}".format(
+		'Device', 'Interface', 'RxGB', 'TxGB')
+	for interface, devicedic in group.iteritems():
+		for devicename, bw in devicedic.iteritems():
+			if devicename != 'total':
+				print "{0} 	{1} 		{2} 	{3}".format(
+					devicename, interface, bw.rxgb, bw.txgb)
+		print "Total received: {0} gb\nTotal sent: 	{1} gb\n".format(
+			group[interface]['total'].rxgb, 
+			group[interface]['total'].txgb)
+
+def sum_dict(group_calc):
+	for interface, dic in group_calc.iteritems():
+		txgb = 0
+		rxgb = 0
+		for name, bw in dic.iteritems():
+			txgb =+ bw.txgb
+			rxgb =+ bw.rxgb
+		group_calc[interface]['total'] = Bandwidth(rxgb, txgb)
+	return group_calc
+
+def calc_bandwidth_group(groupname):
+	config = read_config()
+	group_calc = {}
+	try: 
+		if not config['groups']:
+			raise KeyError
+		group = config['groups'][groupname]
+		for devicename, devicedic in group.iteritems():
+			print "Fething data for {}...".format(devicename)
+			device = calc_bandwidth_device(devicename)
+			for interface, bw in device.iteritems():
+				group_calc.setdefault(interface, {}).update({devicename:bw})
+		calc_total = sum_dict(group_calc)
+		return calc_total
+	except KeyError:
+		print "Error: Couldn't find the group '{0}'".format(groupname)
+
+def print_bandwidth_device(devicename, start=None, end=None):
+	config = read_config()
+	# Note: Make time amends.
+	calc = calc_bandwidth_device(devicename)	
+
+	print devicename
+	print "{0} 	{1} 	{2} 	{3}".format(
+		'', 'Interface', 'RxGB', 'TxGB')
+	for interface, bw in calc.iteritems():
+		print "{0} 	{1} 		{2} 	{3}".format('', interface, bw.rxgb, bw.txgb)
+
+
+def calc_bandwidth_device(devicename):
+	config = read_config()
+	config['current_device'] = config['devices'][devicename]['_id'] 
+	try: 
+		bandwidth_resp = bandwidth_response(config)
+		device_bandwidth = {}
+		for interface in bandwidth_resp[0]['tree']:
+			device_bandwidth[interface['name']] = calc_bandwidth_interface(
+									interface['tree'])
+		return device_bandwidth
+	except KeyError:
+		sys.exit("Error: {0} doesn't have any data for this period".format(devicename))
+
+
+def calc_bandwidth_interface(device_bandwidth):
+	"""Takes input from device_bandwidth, for all network interfaces."""
+	#NOTE: need to adjust this for several eth. 
+	# second list is the interface. for 
+	rxmblist = device_bandwidth[0]['data']
+	txmblist = device_bandwidth[1]['data']
 
 	# There two definitions though
 	# http://en.wikipedia.org/wiki/Gigabyte#Definition
 
-	rxgb = sum([value['y'] for value in rxmblist])/1024
-	txgb = sum([value['y'] for value in txmblist])/1024
-	Bandwidth = namedtuple('Bandwidth', 'rxgb, txgb')
+	rxgb = round(sum([value['y'] for value in rxmblist])/1024)
+	txgb = round(sum([value['y'] for value in txmblist])/1024)
 
 	return Bandwidth(rxgb, txgb)
 
 if __name__ == '__main__':
 	config = read_config()
-	devices = available_devices()
-	metrics = available_metrics()
-	bandwidth = bandwidth_response()
+	#devices = available_devices()
+	#metrics = available_metrics()
+	#bandwidth = bandwidth_response(config)
 
-	device_names = get_devices(devices)
-	adapters = get_network_interfaces(metrics)
-	total_bandwidth = calculate_bandwidth(bandwidth)
+	#device_names = get_devices(devices)
+	#adapters = get_network_interfaces(metrics)
+	#total_bandwidth = calculate_bandwidth(bandwidth)
 
 
