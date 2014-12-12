5848217c262da3e0bd5f6abde448af6a2975d6d0
Jonathan Sundqvist <sundqvist.jonathan@gmail.com>
2014-12-12 11:30:42 +0000

    Minor changes to setup.py

--- setup.py
+++ setup.py
@@ -10,12 +10,13 @@ install_reqs = parse_requirements('requirements.txt')
 reqs = [str(ir.req) for ir in install_reqs]
 
 setup(name='sd-bandwidth',
+      packages=['bandwidth'],
 	  version='0.12',
 	  description='Command line interface to get bandwidth for a device and interface',
 	  author='Jonathan Sundqvist',
 	  author_email='jonathan@serverdensity.com',
-	  url='',
-	  packages=find_packages(),
+      keywords=['monitoring', 'serverdensity'],
+	  url='https://github.com/serverdensity/sd-bw',
 	  scripts=['sdbw'],
 	  install_requires=reqs,
 	  data_files=[(os.path.expanduser(CONFIG_PATH), ['.config.json'])]
